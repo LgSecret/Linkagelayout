d1fbd6e17088383ef2db3bf186a83360ba1ade92
LgSecret <lg_secret520@163.com>
2021-06-11 16:20:20 +0800

    添加注释

--- app/src/main/java/com/example/linkagelayout/ContentAdapter.java
+++ app/src/main/java/com/example/linkagelayout/ContentAdapter.java
@@ -81,11 +81,17 @@ public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ItemView
     //多条recycleview联动
     public void initRecyclerView(RecyclerView recyclerView) {
         recyclerView.setHasFixedSize(true);
+        //为每一个recycleview创建layoutManager
         LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        //todo
+        // 通过移动layoutManager来实现列表滑动  此行是让新加载的item条目保持跟已经滑动的recycleview位置保持一致
+        // 也就是上拉加载更多的时候  保证新加载出来的item 跟已经滑动的item位置保持一致
         if (layoutManager != null && firstPos > 0 && firstOffset > 0) {
             layoutManager.scrollToPositionWithOffset(firstPos + 1, firstOffset);
         }
+        // 添加所有的 recyclerView
         observerList.add(recyclerView);
+        //当触摸条目的时候 停止滑动
         recyclerView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -99,21 +105,26 @@ public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ItemView
                 return false;
             }
         });
+        //添加当前滑动recycleview的滑动监听
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
                 LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                //获取显示第一个item的位置
                 int firstPos1 = linearLayoutManager.findFirstVisibleItemPosition();
                 View firstVisibleItem = linearLayoutManager.getChildAt(0);
                 if (firstVisibleItem != null) {
+                    //获取第一个item的偏移量
                     int firstRight = linearLayoutManager.getDecoratedRight(firstVisibleItem);
+                    //遍历其它的所有的recycleview条目
                     for (RecyclerView rv : observerList) {
                         if (recyclerView != rv) {
                             LinearLayoutManager layoutManager = (LinearLayoutManager) rv.getLayoutManager();
                             if (layoutManager != null) {
                                 firstPos = firstPos1;
                                 firstOffset = firstRight;
+                                //通过当前显示item的位置和偏移量的位置来置顶recycleview 也就是同步其它item的移动距离
                                 layoutManager.scrollToPositionWithOffset(firstPos + 1, firstRight);
                             }
                         }
